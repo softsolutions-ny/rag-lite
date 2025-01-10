e0c7b5e00affe52c6b1dbe85e455e82ab037d9e8
arekhalpern <arek.halpern@gmail.com>
2025-01-09 23:18:21 -0500

    Initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="justify-self-start text-lg">RAGlite</div>
+      <p>The light weight RAG app</p>
+    </div>
+  );
+}
