a23441a164d4d5eecaba3f5da54f960ec89652a0
arekhalpern <arek.halpern@gmail.com>
2025-01-29 13:05:19 -0500

    build script adjusted

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")/.."
 
 # Install Python dependencies
 echo "Installing dependencies..."
-pip install -r requirements.txt
+pip install -r ./requirements/prod.txt
 
 # Run database migrations
 echo "Running database migrations..."
