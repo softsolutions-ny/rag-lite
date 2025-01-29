598238d6f6e9cd7ee93ba4586a396f4b42cb9c1e
arekhalpern <arek.halpern@gmail.com>
2025-01-28 22:45:47 -0500

    chatcontainer wrapped in suspense

--- frontend/app/dashboard/chat/page.tsx
+++ frontend/app/dashboard/chat/page.tsx
@@ -1,10 +1,13 @@
 import { ChatContainer } from "@/components/chat/chat-container";
+import { Suspense } from "react";
 
 export default function ChatPage() {
   return (
     <main className="container mx-auto flex flex-col gap-8 p-8">
       <div className="flex flex-col gap-2"></div>
-      <ChatContainer />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ChatContainer />
+      </Suspense>
     </main>
   );
 }
